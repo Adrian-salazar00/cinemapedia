331c0f90c36edab52201142fcc5fc13b67377495
Adrian Salazar <agsb.salazar15@gmail.com>
2023-11-06 18:03:13 -0500

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+class AppTheme {
+
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Color.fromARGB(95, 52, 99, 255)
+  );
+
+
+}
